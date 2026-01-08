904b21d5e06af3ec5aef54e18a2cecabde5ef2ab
Ronaldmolinares <108416072+Ronaldmolinares@users.noreply.github.com>
2026-01-07 19:41:44 -0500

    método burbuja

---
+++ 08_burbuja.py
@@ -0,0 +1,22 @@
+import random
+
+
+def burbuja(lista):
+    n = len(lista)
+    intercambios = 0
+    for pasada in range(n):
+        for j in range(0, n - pasada - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                intercambios += 1
+    return lista, intercambios
+
+
+if __name__ == "__main__":
+    tamaño_lista = int(input("Tamaño de la lista: "))
+
+    lista = [random.randint(0, 100) for _ in range(tamaño_lista)]
+    print(f"Lista original:\n{lista}")
+
+    lista_ordenada, intercambios = burbuja(lista)
+    print(f"\nLista ordenada: {lista_ordenada}\nNúmero de intercambios: {intercambios}")
