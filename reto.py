4553f551c0cd97f39e0013f9d84ec2ea73f9b9a7
Ronaldmolinares <108416072+Ronaldmolinares@users.noreply.github.com>
2026-01-11 20:48:54 -0500

    practica del ordenamiento por mezcla

--- reto.py
+++ reto.py
@@ -1,6 +1,42 @@
 import random
 
 
+def ordenamiento_mezcla(lista):
+    if len(lista) > 1:
+        mitad = len(lista) // 2
+        izquierda = lista[:mitad]
+        derecha = lista[mitad:]
+
+        ordenamiento_mezcla(izquierda)
+        ordenamiento_mezcla(derecha)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(izquierda) and j < len(derecha):
+            if izquierda[i] < derecha[j]:
+                lista[k] = izquierda[i]
+                i += 1
+                k += 1
+            else:
+                lista[k] = derecha[j]
+                j += 1
+                k += 1
+
+        while i < len(izquierda):
+            lista[k] = izquierda[i]
+            i += 1
+            k += 1
+
+        while j < len(derecha):
+            lista[k] = derecha[j]
+            j += 1
+            k += 1
+
+    return lista
+
+
 def numero_mayor(lista):
     resultado = lista[0]
 
